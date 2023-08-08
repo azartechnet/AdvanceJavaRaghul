541afba3f2a7546ea108f93e6a00064a6fed329b
MohamedAzarudeen <azartechnet@gmail.com>
2023-08-08 16:22:15 +0530

    Initial commit

---
+++ ProjectP1/src/main/java/Servlet1.java
@@ -0,0 +1,42 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet 
+{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		PrintWriter out=resp.getWriter();
+		
+		String u1=req.getParameter("uname");
+		String p1=req.getParameter("pass");
+		if(u1.equals("admin")&&p1.equals("admin"))
+		{
+			RequestDispatcher rd=req.getRequestDispatcher("sucess.html");
+			rd.forward(req, resp);
+			//out.println("LoginSucess!!!");
+		}
+		else
+		{
+			RequestDispatcher rd=req.getRequestDispatcher("index.html");
+			rd.include(req, resp);
+			out.println("LoginFail!!!");
+		}
+		//out.println("Your value is::"+u1+""+p1);
+	}
+
+}
